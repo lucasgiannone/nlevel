2d0ba2be431ff390b58b484620dea3c43670b6ca
darkruun <48525954+darkruun@users.noreply.github.com>
2021-05-14 15:05:15 -0300

    Revert "Revert "Sobre responsivo""
    This reverts commit 518b01fe822fa212c9e9d8cefeb91f6c2f8753f0.

--- sobre.php
+++ sobre.php
@@ -20,94 +20,41 @@ require_once "./components/nav.php"
 <!-- SOBRE -->
 <div id="homepage">
         <div id="heroimg">
-            <div id="herotxt">
-                <img id="iconetv" src="../images/tv.png">
-                <p>Aulas e Palestras</p>
-            </div>
+            <img id="iconetv" src="../images/tv.png"></img>
+                    <h1>Next Level</h1>
+                    <p>Trabalhando de uma forma nunca vista no mercado.</p>
         </div>
-            <div id="sobrepage">
+</div>
+<div id="sobrepage">
 
-                <h1>Como funciona a transmissão</h1>
-                <h1>ao vivo de eventos online</h1>
-            
-                <h2>O que fazemos</h2> 
-                <p>O Next Level é uma plataforma online, que busca conectar estudantes de nível técnico e superior aos congressos,
-                palestras, eventos, feiras tecnológicas e acadêmicas. O Next Level vem para te conectar ao mundo profissional,
-                trazendo os mais importantes eventos acadêmicos e profissionais.
-                </p>
-                 
-                <h2>Missão</h2> 
-                <p>Acreditamos que a informação e o conhecimento devem ser: rápidos, claros e principalmente acessíveis a todos,
-                com isso, nossa missão é criar uma ferramenta prática que possibilite a interação entre as pessoas, universidades
-                e empresas, a qual será importante na sua carreira profissional e fornecerá também o poder de gerenciamento
-                completo das informações dos eventos, otimizando e trabalhando informações de maneira customizada, afim de
-                proporcionar maior força estratégica para nossos clientes.
-                </p> 
-                
-                <h2>Visão</h2> 
-                <p>
-                O Next Level deseja, tornar-se a maior plataforma de divulgação e gerenciamento de eventos acadêmicos e
-                conhecimentos gerais da América do Sul, buscando cada vez mais auxiliar:
-                </p> 
-                <ul>
-                    <li>
-                    O trabalho dos grandes profissionais educadores e desenvolvedores de inovação que necessitam de público para que
-                    assim possam promover suas ideias;
-                    </li> 
-                    <li>
-                    No desenvolvimento do intelecto humano que só tem a ganhar se conectando e aderindo aos educadores e
-                    desenvolvedores de inovações;
-                    </li> 
-                    <li>
-                    No desenvolvimento de melhores profissionais no mercado de trabalho por meio da criação de networking e
-                    multiplicação do conhecimento, que serão incentivados pelo Next Level.
-                    </li>
-                </ul> 
+    <h1>Como funciona a transmissão</h1>
+    <h1>ao vivo de eventos online</h1>
 
-                <h2>Valores</h2> 
-                <ul>
-                    <li>Ética e Transparência</li>
-                    <li>Pessoas</li> 
-                    <li>Cordialidade</li> 
-                    <li>Empreendedorismo e Inovação</li>
-                    <li>Resultados</li> 
-                    <li>Praticidade</li> 
-                    <li>Criatividade</li>
-                </ul> 
-                
-                <h2>Nossa História</h2> 
-                <p>
-                Criado e desenvolvido inicialmente por dois graduandos do curso de Engenharia de Produção em uma instituição da
-                região de Sorocaba, interior paulista. O Next Level surgiu através da demanda de um trabalho acadêmico com tema
-                inovação, do qual inicialmente
-                o atual CEO da empresa, o Sr. Vitor Hugo de Paula Pereira identificou uma necessidade em seu dia a dia que ocorria
-                devido a ineficiência na divulgação de eventos acadêmicos e tecnológicos para o público universitário, afinal de
-                contas, estudar
-                e trabalhar necessita de um grande investimento do seu tempo pessoal, impondo barreiras para que se consiga dar a
-                devida atenção aos eventos que são de nosso interesse profissional. Dessa forma precisava-se de uma ferramenta que
-                fornecesse praticidade
-                na informação da ocorrência de um evento.
-                <br> E assim no início do ano de 2015 surgiu a ideia da criação de um App para a prestação desse tipo de serviço,
-                divulgação de eventos acadêmicos com foco ao público estudantil.
-                <br> Somente no ano de 2017 foi possível lançar o App Next Level, disponível para o Sistema Operacional Android,
-                além de uma plataforma WebApp contendo as mesmas funcionalidades do aplicativo e assim atendendo os usuários do
-                sistema iOS. Desde
-                de 2015 até o ano de lançamento, os graduandos tiveram que desenvolver alguns trabalhos e pesquisas, que os
-                fizessem validar o desenvolvimento desse App assim como a prestação de serviços de nossa empresa. Durante todo
-                esse período o COO da empresa, o
-                Sr. Jânio Carlos Santos Oliveira se fez presente e de suma importância no desenvolvimento e crescimento da empresa
-                Next Level.
-                <br>Para o desenvolvimento e amadurecimento da ideia houve a participação de outros colaboradores dentro da Equipe
-                Next Level que se empenharam no crescimento e aprimoramento de nossa marca. À todos que fizeram e fazem parte
-                dessa equipe, nossos mais sinceros agradecimentos.
-                <br> Atualmente o Next Level vê e busca alcançar inúmeras oportunidades de disseminar seu trabalho, prevendo não
-                ser somente uma aplicação mobile, e sim uma considerável plataforma de prestação de serviços em prol da
-                multiplicação de conhecimento
-                e formação de melhores profissionais no Brasil.
-                </p> 
-                <h4>Se conecte à Next Level</h4> 
-                <p>Satisfatoriamente,</p> <p>Equipe Next Level Ltda.</p> <p>As informações contidas sobre os eventos são de total responsabilidade dos organizadores.</p></div>
-            </div>
+    <h2>Sobre nós</h2> 
+        <p>
+        A NextLevel nasceu no ano de 2020 com o propósito de suprir uma necessidade há tanto tempo já conhecida no setor educacional: um sistema de Ensino a Distância (EAD) e Estratégias de Aprendizagem Remota (EAR) com excelência. Propósito esse que deu origem ao nosso nome (do português Próximo Nível ao inglês NextLevel). O tempo passou, nos aprimoramos e nos tornamos uma organização mais estruturada e objetiva focando no próximo nível, que é a proeminência no desenvolvimento nas áreas atuantes.
+     </p>
+ 
+    <h2>Palavras da equipe NextLevel</h2> 
+        <p>
+        Prover um ambiente de qualidade e de fácil manuseio é o nosso objetivo. Essa plataforma EAR foi projetada para atender as necessidades do mercado destacando-se por sua modernidade e praticidade, com ferramentas tão intuitivas quanto qualitativas.
+    </p> 
+
+    <h2>Missão</h2> 
+        <p>
+        Melhoria contínua através de trabalho ágil e estratégico em nossa plataforma provendo qualidade aos estudantes, professores e possíveis clientes.
+     </p> 
+    <h2>Visão</h2> 
+        <p>
+        Ser referência na disponibilização em SaaS de plataforma de EAR e EAD.
+        </p> 
+
+        <h2>Valores</h2> 
+            <p>
+            Prezar sempre pelo bom atendimento. Exercer nossas atividades com ética, qualidade e respeito. Garantir que os interesses coletivos prevaleçam e conquistar a confiança de nossos colaboradores e clientes.
+        </p> 
+</div>
+
     <!-- FOOTER -->
     <footer>
 		<address class="copyright">Copyright NextLevelCO 2021.</address>
