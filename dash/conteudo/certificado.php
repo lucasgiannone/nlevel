3f42b4c028903144f65c0fb12202a0efeb34db9f
darkruun <48525954+darkruun@users.noreply.github.com>
2021-05-21 13:50:53 -0300

    CSS pagina do certificado manual

--- dash/conteudo/certificado.php
+++ dash/conteudo/certificado.php
@@ -30,42 +30,55 @@
         <!-- App CSS -->  
         <link id="theme-style" rel="stylesheet" href="../assets/css/portal.css">
     </head>
-<!-- <style>
-    .container{
-        background-color:lightskyblue;
-    }
-   .container>h1{
-       text-align: center;
-       color: rgb(30, 57, 210);
-   }
-   .container>label{
-    color: rgb(30, 57, 210);
-   }
-</style> -->
-
 <body class="app">
 <?php require '../components/navbar.php';?>
-<div class="app-wrapper pl-3 pl-md-5">
-        <div class="app-content pt-3">
-            <div class="container row px-md-5">
-                <h1 class="page-title pb-0">Gerador de Certificado</h1>
-                    <!-- CONTEÚDO -->
-        <label>
-            Name:
-            <input id="name" type='text'>
-            <br>
-            Data:
-            <input id="data" type='text'>
-            <br>
-            Palestra:
-            <input id="palestra" type='text'>
-        </label>
-        <a href="#" id="download-btn">Download</a>
-        <canvas id="canvas"height="350px" width="500px"></canvas>
-        <!-- A4 <canvas id="canvas"height="2480px" width="3508px"></canvas> -->
+<div class="app-wrapper">
+    <div class="app-content pt-3 p-md-3 p-lg-4">
+        <div class="card d-flex">
+            <div class="app-card m-4">
+                <div class="row">
+                    <!-- Icon -->
+                    <div class="col-auto">
+                        <div class="app-icon-holder">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#15a362" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z"/>
+                        <path d="M14 3v5h5M16 13H8M16 17H8M10 9H8"/>
+                        </svg>
+                        </div>
+                    </div>
+                    <!-- Title -->
+                    <div class="col-auto">
+                        <h4 class="app-title m-2">Gerador de Certificado</h4>
+                    </div>
+                </div>
+                <!-- Form as table -->
+                    <form method="POST" enctype="multipart/form-data">
+                        <table class="table w-100">
+                            <input name="hidden" type="hidden" value="true">
+                            <tr>
+                                <td><label for="titulo">Nome:</label></td>
+                                <td><input name="nome" id="name" type="text" required></td>        
+                            </tr>
+
+                            <tr>
+                                <td><label for="descricao">Palestra</label></td>
+                                <td><input name="palestra" id="palestra" type="text" required></td>        
+                            </tr>
+
+                            <tr>
+                                <td><label for="datetime">Data</label></td>
+                                <td colspan="1"><input name="datetime" id="data" type="date" required></td>
+                            </tr>
+
+                        </table>
+                                <a href="#" id="download-btn">Download</a>
+                                <canvas id="canvas"height="350px" width="500px"></canvas>
+                                <!-- A4 <canvas id="canvas"height="2480px" width="3508px"></canvas> -->
+                    </form>
             </div>
         </div>
     </div>
+</div>
     <!-- Javascript -->          
     <script src="../assets/plugins/popper.min.js"></script>
     <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>  
